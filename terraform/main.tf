a1c8c1f49d85dd31129979b99fdc8306a473b52b
Smit Waman <159112812+smitwaman@users.noreply.github.com>
2024-02-28 18:26:18 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,35 +1,39 @@
-provider "azurerm" {
-  features {}
+resource "random_pet" "rg_name" {
+  prefix = var.resource_group_name_prefix
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
-  location = var.location
+  location = var.resource_group_location
+  name     = random_pet.rg_name.id
 }
 
-resource "azurerm_virtual_network" "vnat" {
-  name                = var.virtual_network_name
-  address_space       = var.virtual_network_address_space
+# Create virtual network
+resource "azurerm_virtual_network" "my_terraform_network" {
+  name                = "myVnet"
+  address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
 
-resource "azurerm_subnet" "subnet" {
-  name                 = var.subnet_name
+# Create subnet
+resource "azurerm_subnet" "my_terraform_subnet" {
+  name                 = "mySubnet"
   resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnat.name
-  address_prefixes     = var.subnet_address_prefixes
+  virtual_network_name = azurerm_virtual_network.my_terraform_network.name
+  address_prefixes     = ["10.0.1.0/24"]
 }
 
-resource "azurerm_public_ip" "publicip" {
-  name                = var.public_ip_name
+# Create public IPs
+resource "azurerm_public_ip" "my_terraform_public_ip" {
+  name                = "myPublicIP"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = var.public_ip_allocation_method
+  allocation_method   = "Dynamic"
 }
 
-resource "azurerm_network_security_group" "nsg" {
-  name                = var.nsg_name
+# Create Network Security Group and rule
+resource "azurerm_network_security_group" "my_terraform_nsg" {
+  name                = "myNetworkSecurityGroup"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
@@ -46,73 +50,75 @@ resource "azurerm_network_security_group" "nsg" {
   }
 }
 
-resource "azurerm_network_interface" "netint" {
-  name                = var.network_interface_name
+# Create network interface
+resource "azurerm_network_interface" "my_terraform_nic" {
+  name                = "myNIC"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
   ip_configuration {
-    name                          = var.ip_configuration_name
-    subnet_id                     = azurerm_subnet.subnet.id
-    private_ip_address_allocation = var.private_ip_allocation
-    public_ip_address_id          = azurerm_public_ip.publicip.id
+    name                          = "my_nic_configuration"
+    subnet_id                     = azurerm_subnet.my_terraform_subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.my_terraform_public_ip.id
   }
 }
 
-
-
-resource "tls_private_key" "az_ssh" {
-    algorithm = "RSA"
-    rsa_bits = 4096
+# Connect the security group to the network interface
+resource "azurerm_network_interface_security_group_association" "example" {
+  network_interface_id      = azurerm_network_interface.my_terraform_nic.id
+  network_security_group_id = azurerm_network_security_group.my_terraform_nsg.id
 }
 
+# Generate random text for a unique storage account name
+resource "random_id" "random_id" {
+  keepers = {
+    # Generate a new ID only when a new resource group is defined
+    resource_group = azurerm_resource_group.rg.name
+  }
 
-resource "null_resource" "set_permissions" {
-  provisioner "local-exec" {
-    command = "chmod 0600 ~/.ssh/*.pem"
-  }
-  
-  # Run this provisioner when the resource is created
-  triggers = {
-    always_run = "${timestamp()}"
-  }
+  byte_length = 8
 }
 
+# Create storage account for boot diagnostics
+resource "azurerm_storage_account" "my_storage_account" {
+  name                     = "diag${random_id.random_id.hex}"
+  location                 = azurerm_resource_group.rg.location
+  resource_group_name      = azurerm_resource_group.rg.name
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
 
+# Create virtual machine
+resource "azurerm_linux_virtual_machine" "my_terraform_vm" {
+  name                  = "myVM"
+  location              = azurerm_resource_group.rg.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  network_interface_ids = [azurerm_network_interface.my_terraform_nic.id]
+  size                  = "Standard_B1s"
 
-resource "azurerm_linux_virtual_machine" "vm" {
-  
-  name                = "ansible-demo"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  size                = var.vm_size
-  admin_username      = "adminuser"
-  network_interface_ids = [
-    azurerm_network_interface.netint.id,
-  ]
-    disable_password_authentication = true
-
-
-
-    admin_ssh_key {
-        username = "azureuser"
-        
-        public_key = tls_private_key.az_ssh.private_key_openssh #The magic here
-    }
-
-    tags = {
-        environment = "Terraform Demo"
-    }
-
-os_disk {
+  os_disk {
+    name                 = "myOsDisk"
     caching              = "ReadWrite"
-    storage_account_type = var.os_disk_storage_account_type
+    storage_account_type = "Premium_LRS"
   }
 
   source_image_reference {
     publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts-gen2"
     version   = "latest"
+  }
+
+  computer_name  = "hostname"
+  admin_username = var.username
+
+  admin_ssh_key {
+    username   = var.username
+    public_key = jsondecode(azapi_resource_action.ssh_public_key_gen.output).publicKey
+  }
+
+  boot_diagnostics {
+    storage_account_uri = azurerm_storage_account.my_storage_account.primary_blob_endpoint
   }
 }

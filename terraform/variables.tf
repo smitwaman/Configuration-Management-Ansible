fd34e671cc88643ca01464ab4a11f8badc4ca954
Smit Waman <159112812+smitwaman@users.noreply.github.com>
2024-02-28 18:27:01 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,71 +1,17 @@
-variable "resource_group_name" {
-  description = "Name of the resource group"
+variable "resource_group_location" {
+  type        = string
+  default     = "eastus"
+  description = "Location of the resource group."
 }
 
-variable "location" {
-  description = "Location for all resources"
-  default     = "East US 2"
+variable "resource_group_name_prefix" {
+  type        = string
+  default     = "rg"
+  description = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
 }
 
-variable "virtual_network_name" {
-  description = "Name of the virtual network"
-}
-
-variable "virtual_network_address_space" {
-  description = "Address space for the virtual network"
-  type        = list(string)
-}
-
-variable "subnet_name" {
-  description = "Name of the subnet"
-}
-
-variable "subnet_address_prefixes" {
-  description = "Address prefixes for the subnet"
-  type        = list(string)
-}
-
-variable "public_ip_name" {
-  description = "Name of the public IP"
-}
-
-variable "public_ip_allocation_method" {
-  description = "Allocation method for the public IP"
-  default     = "Dynamic"
-}
-
-variable "nsg_name" {
-  description = "Name of the network security group"
-}
-
-variable "network_interface_name" {
-  description = "Name of the network interface"
-}
-
-variable "ip_configuration_name" {
-  description = "Name of the IP configuration"
-}
-
-variable "private_ip_allocation" {
-  description = "Allocation method for the private IP"
-  default     = "Dynamic"
-}
-
-variable "vm_name" {
-  description = "Name of the virtual machine"
-}
-
-variable "vm_size" {
-  description = "Size of the virtual machine"
-}
-
-variable "vm_admin_username" {
-  description = "Admin username for the virtual machine"
-}
-
-
-
-variable "os_disk_storage_account_type" {
-  description = "Storage account type for the OS disk"
-  default     = "Standard_LRS"
+variable "username" {
+  type        = string
+  description = "The username for the local account that will be created on the new VM."
+  default     = "azureadmin"
 }

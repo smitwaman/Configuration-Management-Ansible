065426bc2de8f02a1a94ebbfb390647c9c2cea02
Smit Waman <159112812+smitwaman@users.noreply.github.com>
2024-02-28 15:02:38 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -64,7 +64,7 @@ variable "vm_admin_username" {
 }
 
 variable "ssh_public_key_path" {
-  description = "C:\Users\Sparx\Downloads\devops.pem"
+  description = "Path_to_key"
 }
 
 variable "os_disk_storage_account_type" {

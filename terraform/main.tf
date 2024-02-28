f286c91ac30a9b6fe722ba58c09b0581f83e693b
Smit Waman <159112812+smitwaman@users.noreply.github.com>
2024-02-28 13:57:32 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,176 +1,87 @@
-resource "azurerm_resource_group" "test" {
-  name = "acctestrg"
-  location = "West US 2"
+provider "azurerm" {
+  features {}
 }
 
-resource "azurerm_virtual_network" "test" {
-  name = "acctvn"
-  address_space = [
-    "10.0.0.0/16"]
-  location = "${azurerm_resource_group.test.location}"
-  resource_group_name = "${azurerm_resource_group.test.name}"
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
 }
 
-resource "azurerm_subnet" "test" {
-  name = "acctsub"
-  resource_group_name = "${azurerm_resource_group.test.name}"
-  virtual_network_name = "${azurerm_virtual_network.test.name}"
-  address_prefix = "10.0.2.0/24"
+resource "azurerm_virtual_network" "vnat" {
+  name                = var.virtual_network_name
+  address_space       = var.virtual_network_address_space
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 }
 
-resource "azurerm_public_ip" "test" {
-  name = "publicIPForLB"
-  location = "${azurerm_resource_group.test.location}"
-  resource_group_name = "${azurerm_resource_group.test.name}"
-  allocation_method       = "Static"
-  idle_timeout_in_minutes = 30
-  domain_name_label="dellemcappdemo"
-tags = {
-    Name = "MyNSG"
-    Environment = "Staging"
-    // Add other tags as needed
+resource "azurerm_subnet" "subnet" {
+  name                 = var.subnet_name
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnat.name
+  address_prefixes     = var.subnet_address_prefixes
+}
+
+resource "azurerm_public_ip" "publicip" {
+  name                = var.public_ip_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = var.public_ip_allocation_method
+}
+
+resource "azurerm_network_security_group" "nsg" {
+  name                = var.nsg_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
   }
-
 }
 
-resource "azurerm_network_security_group" "test" {
-  name                = "acceptanceTestSecurityGroup1"
-  location            = "${azurerm_resource_group.test.location}"
-  resource_group_name = "${azurerm_resource_group.test.name}
-}
+resource "azurerm_network_interface" "netint" {
+  name                = var.network_interface_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 
-resource "azurerm_network_security_rule" "test1" {
-  name                        = "SSH"
-  priority                    = 340
-  direction                   = "Inbound"
-  access                      = "Allow"
-  protocol                    = "*"
-  source_port_range           = "*"
-  destination_port_range      = "22"
-  source_address_prefix       = "*"
-  destination_address_prefix  = "*"
-  resource_group_name         = "${azurerm_resource_group.test.name}"
-  network_security_group_name = "${azurerm_network_security_group.test.name}"
-tags = {
-    Name = "MyNSG"
-    Environment = "Production"
-    // Add other tags as needed
-  }
-
-}
-
-resource "azurerm_network_security_rule" "test2" {
-  name                        = "8080"
-  priority                    = 1020
-  direction                   = "Inbound"
-  access                      = "Allow"
-  protocol                    = "Tcp"
-  source_port_range           = "*"
-  destination_port_range      = "8080"
-  source_address_prefix       = "*"
-  destination_address_prefix  = "*"
-  resource_group_name         = "${azurerm_resource_group.test.name}"
-  network_security_group_name = "${azurerm_network_security_group.test.name}"
-}
-
-
-
-
-resource "azurerm_network_interface" "test" {
-  count = 1
-  name = "acctni${count.index}"
-  location = "${azurerm_resource_group.test.location}"
-  resource_group_name = "${azurerm_resource_group.test.name}"
-  network_security_group_id = "${azurerm_network_security_group.test.id}"
   ip_configuration {
-    name = "testConfiguration"
-    subnet_id = "${azurerm_subnet.test.id}"
-    private_ip_address_allocation = "dynamic"
-    public_ip_address_id = "${azurerm_public_ip.test.id}"
+    name                          = var.ip_configuration_name
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = var.private_ip_allocation
+    public_ip_address_id          = azurerm_public_ip.public.id
   }
 }
 
-resource "azurerm_managed_disk" "test" {
-  count = 1
-  name = "datadisk_existing_${count.index}"
-  location = "${azurerm_resource_group.test.location}"
-  resource_group_name = "${azurerm_resource_group.test.name}"
-  storage_account_type = "Standard_LRS"
-  create_option = "Empty"
-  disk_size_gb = "1023"
-}
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = var.vm_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = var.vm_size
+  admin_username      = var.vm_admin_username
+  network_interface_ids = [
+    azurerm_network_interface.netint.id,
+  ]
+  admin_ssh_key {
+    username   = var.vm_admin_username
+    public_key = var.ssh_public_key_path
+  }
 
-resource "azurerm_availability_set" "avset" {
-  name = "avset"
-  location = "${azurerm_resource_group.test.location}"
-  resource_group_name = "${azurerm_resource_group.test.name}"
-  platform_fault_domain_count = 2
-  platform_update_domain_count = 2
-  managed = true
-}
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = var.os_disk_storage_account_type
+  }
 
-resource "azurerm_virtual_machine" "test" {
-  count = 1
-  name = "dellemc_demo"
-  location = "${azurerm_resource_group.test.location}"
-  availability_set_id = "${azurerm_availability_set.avset.id}"
-  resource_group_name = "${azurerm_resource_group.test.name}"
-  network_interface_ids = ["${azurerm_network_interface.test.id}"]
-  vm_size = "Standard_DS1_v2"
-
-  # Uncomment this line to delete the OS disk automatically when deleting the VM
-  # delete_os_disk_on_termination = true
-
-  # Uncomment this line to delete the data disks automatically when deleting the VM
-  # delete_data_disks_on_termination = true
-
-  storage_image_reference {
+  source_image_reference {
     publisher = "Canonical"
-    offer = "UbuntuServer"
-    sku = "16.04-LTS"
-    version = "latest"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
   }
-
-  storage_os_disk {
-    name = "myosdisk${count.index}"
-    caching = "ReadWrite"
-    create_option = "FromImage"
-    managed_disk_type = "Standard_LRS"
-  }
-
-  # Optional data disks
-  storage_data_disk {
-    name = "datadisk_new_${count.index}"
-    managed_disk_type = "Standard_LRS"
-    create_option = "Empty"
-    lun = 0
-    disk_size_gb = "1023"
-  }
-
-  storage_data_disk {
-    name = "${element(azurerm_managed_disk.test.*.name, count.index)}"
-    managed_disk_id = "${element(azurerm_managed_disk.test.*.id, count.index)}"
-    create_option = "Attach"
-    lun = 1
-    disk_size_gb = "${element(azurerm_managed_disk.test.*.disk_size_gb, count.index)}"
-  }
-
-  os_profile {
-    computer_name = "dellemcdemovm"
-    admin_username = "testadmin"
-    admin_password = "Password1234!"
-  }
-
-  os_profile_linux_config {
-    disable_password_authentication = false
-  }
-
-  tags {
-    environment = "staging"
-  }
-}
-
-
-output "public_ip_address" {
-  value = "${azurerm_public_ip.test.ip_address}"
 }

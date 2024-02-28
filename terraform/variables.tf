4fdcc6c5075c944d873b6081fdbcee04d92c262a
Smit Waman <159112812+smitwaman@users.noreply.github.com>
2024-02-28 14:26:19 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -64,7 +64,7 @@ variable "vm_admin_username" {
 }
 
 variable "ssh_public_key_path" {
-  description = "Path to the SSH public key"
+  description = "~/.ssh/"
 }
 
 variable "os_disk_storage_account_type" {
